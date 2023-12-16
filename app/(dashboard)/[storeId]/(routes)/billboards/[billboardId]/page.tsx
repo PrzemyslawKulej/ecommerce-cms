9b21dacc992d1680a262c0b4c8ab54b583e9ad93
pkulej <przemek.kulej1@gmail.com>
2023-12-16 18:25:17 +0100

    feat(billboard-form.tsx): add BillboardForm component to handle creation and editing of billboards feat(billboard-form.tsx): implement form validation using zod schema feat(billboard-form.tsx): add form fields for label and background image URL feat(billboard-form.tsx): implement form submission and update functionality feat(billboard-form.tsx): implement billboard deletion functionality feat(page.tsx): import BillboardForm component and pass initialData prop fix(layout.tsx): import prismadb and assign billboard variable feat(image-upload.tsx): create ImageUpload component for handling image uploads feat(next.config.js): add cloudinary domain to images configuration

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,7 +1,25 @@
+import prismadb from "@/lib/prismadb";
 import React from "react";
+import { BillboardForm } from "./components/billboard-form";
 
-const BillboardPage = () => {
-  return <div>This is a form for billboards</div>;
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  const billboard =
+    (await prisma?.billboard.findUnique({
+      where: {
+        id: params.billboardId,
+      },
+    })) ?? null;
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8">
+        <BillboardForm initialData={billboard} />
+      </div>
+    </div>
+  );
 };
-
 export default BillboardPage;
