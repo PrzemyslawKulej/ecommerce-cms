170ad24069065c8b2d6ae7a75889a381f2fecb7b
pkulej <przemek.kulej1@gmail.com>
2024-02-06 00:33:24 +0100

    fixed bug with middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 import { MiddlewareNotFoundError } from "next/dist/shared/lib/utils";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+  publicRoutes: ["/api/:path*", "/((?!api).*)"],
 });
+
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
